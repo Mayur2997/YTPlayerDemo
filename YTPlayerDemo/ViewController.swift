be737f917544c4f35d99c14b930678954cfa1f9a
Mayur Parmar <mrparmar2997@gmail.com>
2020-06-05 12:11:04 +0530

    Initial Commit

---
+++ YTPlayerDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YTPlayerDemo
+//
+//  Created by Mayur Parmar on 05/06/20.
+//  Copyright © 2020 Mayur Parmar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
